bef5b164cd4ece774d0cfaa8d8587534dfe22846
GrismaShrestha <np03cs4s220099@heraldcollege.edu.np>
2024-04-04 18:49:07 +0545

    Backend: Added nodemon

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,7 +4,6 @@ import { db } from "./db.js";
 const app = express();
 const port = 3000;
 
-// db.query("INSERT INTO test VALUES (NULL, 'foo');");
 const [results] = await db.query("SELECT * FROM test;");
 console.log(results);
 
